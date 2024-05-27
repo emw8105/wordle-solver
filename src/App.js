9a3f2bb1e331069cb4f3e38ad58f01585fa31ab9
Evan Wright <emw8105@gmail.com>
2024-05-27 02:49:39 -0500

    super messy push for dictionary saving

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+// WordBox component
+function WordBox({ letter }) {
+  return <div className="word-box">{letter}</div>;
+}
+
+// WordRow component
+function WordRow({ word }) {
+  return (
+    <div className="word-row">
+      {word.split('').map((letter, index) => (
+        <WordBox key={index} letter={letter} />
+      ))}
+    </div>
+  );
+}
+
+// Main App component
 function App() {
+  const [guesses, setGuesses] = React.useState(['not-found', 'found-yellow', 'found-green']);
+  const [dictionary, setDictionary] = useState([]);
+
+  useEffect(() => {
+    const loadDictionary = async () => {
+      const response = await axios.get('/data/dictionary.txt');
+      const words = response.data.split('\n');
+      setDictionary(words);
+    };
+
+    loadDictionary();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {guesses.map((guess, index) => (
+        <WordRow key={index} word={guess} />
+      ))}
     </div>
   );
 }
